1a9be42310eeedc115c77dd507661168140a7e6b
theKonon <thekonon@gmail.com>
2022-10-10 11:08:36 +0200

    Run main.py for quizz

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-import sys
-from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QApplication, QLabel
-from PySide6.QtGui import QPalette
-                                                     
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    
-    label = QLabel("Hello World", alignment=Qt.AlignCenter)
-    my_BG = QPalette('red')
-    label.setPalette(my_BG)
-    label.show()
-    sys.exit(app.exec())
+import quizz
+Quizz = quizz.NJQuizz()
+Quizz.quizzStart()
